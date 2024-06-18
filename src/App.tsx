42c14db488eae79ef2cd7e06fd16484bbf1f18d3
R <sankaralingam.r@capgemini.com>
2024-06-18 19:13:08 +0530

    Social login integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { IResolveParams, LoginSocialLinkedin } from 'reactjs-social-login';
+import { LinkedInLoginButton } from 'react-social-login-buttons';
+
 
 function App() {
+  const [provider, setProvider] = useState('');
+  const [profile, setProfile] = useState<any>();
+
+  const onLoginStart = useCallback(() => {
+    alert('login start');
+  }, []);
+  
+  const onLogoutSuccess = useCallback(() => {
+    setProfile(null);
+    setProvider('');
+    alert('logout success');
+  }, []);
+
+  const onLogout = useCallback(() => {}, []);
+  const REDIRECT_URI ='https://plenty-planets-beam-42-118-51-2.loca.lt/account/login';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='container'>
+     <LoginSocialLinkedin
+          client_id={process.env.REACT_APP_LINKEDIN_APP_ID || ''}
+          client_secret={process.env.REACT_APP_LINKEDIN_APP_SECRET || ''}
+          redirect_uri={REDIRECT_URI}
+          onLoginStart={onLoginStart}
+          onResolve={({ provider, data }: IResolveParams) => {
+            setProvider(provider);
+            setProfile(data);
+          }}
+          onReject={(err: any) => {
+            console.log(err);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <LinkedInLoginButton />
+        </LoginSocialLinkedin>
     </div>
   );
 }
